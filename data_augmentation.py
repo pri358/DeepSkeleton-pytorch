e48beea816be2b7a335b1edfd23522d2c697fd1d
Priyanshi Jain <priyanshi.jain030@gmail.com>
2021-01-05 14:01:39 +0530

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -4,34 +4,63 @@ import os
 import numpy as np
 from tqdm import tqdm
 from scipy.io import loadmat
+import cv2
+from scipy.ndimage.morphology import distance_transform_edt as bwdist
 
-rootDirGt = "SK-LARGE/groundTruth/train/"
+
+rootDirGt = "/content/drive/MyDrive/IP - 7th sem/SK-SMALL/SK506/groundTruth/train/"
 listData = sorted(os.listdir(rootDirGt))
 
-output_dir = "SK-LARGE/aug_data/ed_scale/"
-img_dir = "SK-LARGE/aug_data/img_scale"
+trainDirImg = "/content/drive/MyDrive/IP - 7th sem/SK-SMALL/SK506/images/train/"
+listimages = sorted(os.listdir(trainDirImg))
+
+output_dir = "/content/drive/My Drive/IP - 7th sem/SK-SMALL/SK506/aug_data/groundTruth/"
+img_dir = "/content/drive/My Drive/IP - 7th sem/SK-SMALL/SK506/aug_data/images/"
+
+os.makedirs(output_dir, exist_ok=True)
+os.makedirs(img_dir, exist_ok=True)
+
+file1 = open("/content/drive/My Drive/IP - 7th sem/SK-SMALL/SK506/aug_data/pairList.txt", 'w') 
 for scale in tqdm([0.8, 1, 1.2]):
-    os.makedirs(output_dir + str(scale) + "/o/", exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(img_dir, exist_ok=True)
     for angle in tqdm([0, 90, 180, 270]):
-        os.makedirs(output_dir + str(scale) + "/o/" + str(angle) + "/f/", exist_ok=True)
+        # os.makedirs(output_dir + str(scale) + "/o/" + str(angle) + "/f/", exist_ok=True)
+        # os.makedirs(img_dir + str(scale) + "/o/" + str(angle) + "/f/", exist_ok=True)
         for flip in tqdm([0, 1, 2]):
-            os.makedirs(output_dir + str(scale) + "/o/" + str(angle) + "/f/" + str(flip), exist_ok=True)
-            for targetName in tqdm(listData):
-                path = output_dir + str(scale) + "/o/" + str(angle) + "/f/" + str(flip) + "/"
+            # os.makedirs(output_dir + str(scale) + "/o/" + str(angle) + "/f/" + str(flip), exist_ok=True)
+            # os.makedirs(img_dir + str(scale) + "/o/" + str(angle) + "/f/" + str(flip), exist_ok=True)
+            for i in range(len(listData)):
+                targetName = listData[i]
+                gtpath = output_dir + str(scale) + str(angle) + str(flip) + "_"
+                impath = img_dir + str(scale) + str(angle) + str(flip) + "_"
                 name = targetName.replace(".mat", ".png")
                 
+                imageTrain = cv2.imread(trainDirImg + listimages[i])
                 itemGround = loadmat(rootDirGt + targetName)
-                edge = (itemGround['edge']).astype(float)*255.0
-                img = (edge).astype(np.uint8)
-                img = Image.fromarray(img, 'L')
+                edge, skeleton = itemGround['edge'], itemGround['symmetry']
+                dist = 2*bwdist(1 - edge)
+                appl = np.vectorize(lambda x, y: 0 if y < 0.5 else x)
+                img = appl(dist,skeleton)
+                img = Image.fromarray(img.astype(np.uint8))
                 img = img.rotate(angle, PIL.Image.BICUBIC, True)
+                imageTrain = Image.fromarray(imageTrain)
+                imageTrain = imageTrain.rotate(angle, PIL.Image.BICUBIC, True)
                 new_size = (int(np.ceil(scale*img.size[0])), int(np.ceil(scale*img.size[1])))
                 img = img.resize(new_size)
+
+                new_size_train = (int(np.ceil(scale*imageTrain.size[0])), int(np.ceil(scale*imageTrain.size[1])))
+                imageTrain = imageTrain.resize(new_size_train)
                 if flip == 1:
                     img = img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+                    imageTrain = imageTrain.transpose(PIL.Image.FLIP_LEFT_RIGHT)
                 elif flip == 2:
                     img = img.transpose(PIL.Image.FLIP_TOP_BOTTOM)
-                img.save(path + name)
+                    imageTrain = imageTrain.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+                img.save(gtpath + name)
+                imageTrain.save(impath+ listimages[i])
+
+                file1.write(impath+ listimages[i] + "," + gtpath + name + "\n")
                 """
                 ground = Image.open(path + name)
                 img = Image.open(path.replace("ed_scale","im_scale") + name.replace(".png", ".jpg"))
